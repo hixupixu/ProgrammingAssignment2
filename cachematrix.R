242cfcf825f9ff941c02fe7c8b00c6022484fd7c
Tytus Sawa <tytus.sawa@gmail.com>
2015-08-23 23:34:12 +0200

    Hala Madrid git push

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
-## Write a short comment describing this function
+HELLO! Unfortunatelly I didn't have time this week to finish the assignment fully, I just uploaded this to get some points.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+Peace!
+
